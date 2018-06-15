47d17a6d3c3f5e5520a7f9ac2f199a2329d6a6ce
aidrifter <aidrifter@gmail.com>
2018-06-15 11:58:27 +0800

    [Add] Generate main.c quickly
    Add Generate main.c quickly

--- scripts/_notebook.sh
+++ scripts/_notebook.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
+TEMP_PATH=$PWD/main.c
+
 if [ ! -z "$2" ];then
     echo "notebook rw mode"
     vim $note_shell_path/notebook/shell_command/$1.md
@@ -7,6 +9,23 @@ if [ ! -z "$2" ];then
 fi
 
 if [ ! -z "$1" ] ;then
+    if [ 'main' = "$1" ];then
+        # create $TEMP_PATH fastly
+        echo "Quickly Generate $TEMP_PATH"
+        echo "#include <stdio.h>" > $TEMP_PATH
+        echo "#include <stdlib.h>" >> $TEMP_PATH
+        echo "#include <stdint.h>" >> $TEMP_PATH
+        echo "#include <stdbool.h>" >> $TEMP_PATH
+        echo "" >> $TEMP_PATH
+        echo "" >> $TEMP_PATH
+        echo "int main()" >> $TEMP_PATH
+        echo "{" >> $TEMP_PATH
+        echo "" >> $TEMP_PATH
+        echo "    return 0;" >> $TEMP_PATH
+        echo "}" >> $TEMP_PATH
+        vim $TEMP_PATH
+        exit
+    fi
     echo "notebook r mode"
     cat $note_shell_path/notebook/shell_command/$1.md
     exit
