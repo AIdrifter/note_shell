295429a1e7b27961207c2406770ca007aefdc1dd
aidrifter <you@example.com>
2018-11-25 11:17:58 +0800

    [Update] leetcode for linkedlist
    finsih: reverse linkedlist
            delete ith node

--- notebook/C/linkedlist/linkedlist.empty.c
+++ notebook/C/linkedlist/linkedlist.empty.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 
 typedef struct ListNode {
@@ -18,6 +19,19 @@ struct ListNode *makeListFromChars(const char *array, int size) {
     }
     return anchor.next;
 }
+
+struct ListNode *makeListFromInts(const int *array, int size) {
+    struct ListNode anchor  = { .next = NULL }, *curr = &anchor;
+
+    for(int i = 0; i < size; i++) {
+        curr = curr->next = malloc(sizeof(struct ListNode));//Creating lists and adding data is a separate function.
+        curr->val = array[i];
+        curr->next = NULL;
+    }
+    return anchor.next;
+}
+
+
 
 /* Definition for singly-linked list. */
 void deleteNode(struct ListNode* node) {
@@ -38,41 +52,90 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n)
     for (now=head; now!=NULL; now=now->next)
         sum++;
 
-    for (now=head, i=0; i < sum-n  ; prev=now, now=now->next, i++)
+    for (now=head, i=0; i < sum  ; prev=now, now=now->next, i++)
     {
 
         if(n==sum) /* delete node on head */
         {
             free(now);
             head = now->next;
+            return head;
         }
 
         /* delete node in tail */
-        if (i==n)
+        if (i==sum-1)
         {
             prev->next = NULL;
             free(now);
             now = NULL;
+            return head;
         }
         else if(i==sum-n)/* other case */
         {
             prev->next = now->next;
             free(now);
             now = NULL;
+            return head;
         }
     }
     return head;
 }
 
+
 /* reverse linkedlist */
 struct ListNode* reverseList(struct ListNode* head) {
+    if (head==NULL)
+        return head;
 
+     struct ListNode *a0 = NULL, *a1 = NULL, *a2 = NULL;
+     a0 = a1; a1 = head;a2 = head->next;
+
+     while(a1!=NULL)
+     {
+         a1->next = a0;
+         a0 = a1;
+         a1 = a2;
+         if(a2!=NULL)a2 = a2->next;
+     }
+     return a0;
 }
 
-/* Merge Two Sorted Lists */
-struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
 
+uint32_t linkedlist_size(struct ListNode *ptr)
+{
+
+    uint32_t size = 0;
+    for(;ptr!=NULL;ptr=ptr->next)
+        size++;
 }
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
+{
+    uint32_t l1_size = 0;
+    uint32_t l2_size = 0;
+    uint32_t idx1=0 , idx2= 0;
+    l1_size = linkedlist_size(l1);
+    l2_size = linkedlist_size(l2);
+    int *ptr= malloc(sizeof(int)*(l1_size+l2_size));
+
+    while(idx1 != l1_size && idx2 != l2_size)
+    {
+        int i = 0 ;
+        if (l1[idx1].val > l2[idx2].val)
+        {
+            ptr[i] = l2[idx2++].val;
+        }
+        else
+        {
+            ptr[i] = l1[idx1++].val;
+        }
+        i++;
+    }
+
+    struct ListNode *input = makeListFromInts(ptr, (l1_size + l2_size));
+    return input;
+}
+
 
 /* Palindrome Linked List */
 bool isPalindrome(struct ListNode* head) {
