c70b60477873c4aebae54dbc38e448bc65d11d59
aidrifter <aidrifter@gmail.com>
2019-01-21 22:46:04 +0800

    [Update] Top Interview : DP[123/15]
    198.house-robber.cpp
    70.climbing-stairs.cpp

--- notebook/leetcode/dynamic_programming/198.house-robber.cpp
+++ notebook/leetcode/dynamic_programming/198.house-robber.cpp
@@ -16,34 +16,61 @@
  * you from robbing each of them is that adjacent houses have security system
  * connected and it will automatically contact the police if two adjacent
  * houses were broken into on the same night.
- * 
+ *
  * Given a list of non-negative integers representing the amount of money of
  * each house, determine the maximum amount of money you can rob tonight
  * without alerting the police.
- * 
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: [1,2,3,1]
  * Output: 4
  * Explanation: Rob house 1 (money = 1) and then rob house 3 (money =
  * 3).
  * Total amount you can rob = 1 + 3 = 4.
- * 
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: [2,7,9,3,1]
  * Output: 12
  * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house
  * 5 (money = 1).
  * Total amount you can rob = 2 + 9 + 1 = 12.
- * 
- * 
+ *
+ *
  */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        
+        vector<int> C(nums.size(),0);
+
+        if(nums.size()==0)
+            return 0;
+
+        if(nums.size()==1)
+            return nums[0];
+
+        if(nums.size()==2)
+            return max(nums[0],nums[1]);
+
+
+        C[0] = nums[0];
+        C[1] = max(nums[0],nums[1]);
+
+        for(int i=2;i<nums.size();i++)
+        {
+            C[i] = max(C[i-1],C[i-2]+nums[i]);
+        }
+
+        return C[nums.size()-1];
     }
 };
--- notebook/leetcode/dynamic_programming/70.climbing-stairs.cpp
+++ notebook/leetcode/dynamic_programming/70.climbing-stairs.cpp
@@ -12,37 +12,69 @@
  * Testcase Example:  '2'
  *
  * You are climbing a stair case. It takes n steps to reach to the top.
- * 
+ *
  * Each time you can either climb 1 or 2 steps. In how many distinct ways can
  * you climb to the top?
- * 
+ *
  * Note: Given n will be a positive integer.
- * 
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: 2
  * Output: 2
  * Explanation: There are two ways to climb to the top.
  * 1. 1 step + 1 step
  * 2. 2 steps
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: 3
  * Output: 3
  * Explanation: There are three ways to climb to the top.
  * 1. 1 step + 1 step + 1 step
  * 2. 1 step + 2 steps
  * 3. 2 steps + 1 step
- * 
- * 
+ *
+ *
  */
 class Solution {
 public:
     int climbStairs(int n) {
-        
+#if 0
+        int sum;
+        map<int,int> hash_table;
+
+        if(n==1)
+            hash_table[1] = 1;
+        if(n==2)
+            hash_table[2] = 2;
+
+        // n does not exist in hashtable
+        if(hash_table.find(n) == hash_table.end() )
+        {
+            sum  =  climbStairs(n-1) +  climbStairs(n-2);
+            hash_table[n] = sum;
+        }
+
+        return hash_table[n];
+#elif 0
+      if(n==1)
+            return 1;
+      if(n==2)
+            return 2;
+
+      return climbStairs(n-1) + climbStairs(n-2);
+#else
+    vector<int> C(n+1,0);
+    C[1] = 1;
+    C[2] = 2;
+    for(int i=3; i <=n ; i++)
+        C[i] = C[i-1] + C[i-2];
+
+    return C[n];
+#endif
     }
 };
