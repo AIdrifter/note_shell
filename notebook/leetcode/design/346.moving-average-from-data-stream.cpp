3b3d381b27dc94af7590429082b39d0b8990bc68
aidrifter <aidrifter@gmail.com>
2019-02-09 18:13:22 +0800

    [Update] Google: Design [122/54]
    346.moving-average-from-data-stream.cpp

---
+++ notebook/leetcode/design/346.moving-average-from-data-stream.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=346 lang=cpp
+ *
+ * [346] Moving Average from Data Stream
+ *
+ * https://leetcode.com/problems/moving-average-from-data-stream/description/
+ *
+ * algorithms
+ * Easy (64.64%)
+ * Total Accepted:    66.6K
+ * Total Submissions: 103K
+ * Testcase Example:  '["MovingAverage","next","next","next","next"]\n[[3],[1],[10],[3],[5]]'
+ *
+ * Given a stream of integers and a window size, calculate the moving average
+ * of all integers in the sliding window.
+ * 
+ * Example:
+ * 
+ * 
+ * MovingAverage m = new MovingAverage(3);
+ * m.next(1) = 1
+ * m.next(10) = (1 + 10) / 2
+ * m.next(3) = (1 + 10 + 3) / 3
+ * m.next(5) = (10 + 3 + 5) / 3
+ * 
+ * 
+ * 
+ * 
+ */
+class MovingAverage {
+public:
+    /** Initialize your data structure here. */
+    MovingAverage(int size) {
+        
+    }
+    
+    double next(int val) {
+        
+    }
+};
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
