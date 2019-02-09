43f5e1d468b40d1ec419d4b427c369cd5255bd15
aidrifter <aidrifter@gmail.com>
2019-02-09 20:49:34 +0800

    [Update] Google: Design [122/60]
    1. Add numbers from arrayAndString [5]
    2. 346.moving-average-from-data-stream.cpp

--- notebook/leetcode/design/346.moving-average-from-data-stream.cpp
+++ notebook/leetcode/design/346.moving-average-from-data-stream.cpp
@@ -13,29 +13,49 @@
  *
  * Given a stream of integers and a window size, calculate the moving average
  * of all integers in the sliding window.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * MovingAverage m = new MovingAverage(3);
  * m.next(1) = 1
  * m.next(10) = (1 + 10) / 2
  * m.next(3) = (1 + 10 + 3) / 3
  * m.next(5) = (10 + 3 + 5) / 3
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  */
 class MovingAverage {
 public:
+
+    vector<int> S;
+    int window_size = 0;
+    int base_cnt = 0;
+    int idx = 0;
+
     /** Initialize your data structure here. */
     MovingAverage(int size) {
-        
+        window_size = size;
+        S.resize(window_size);
+        return;
     }
-    
+
     double next(int val) {
-        
+        double sum = 0;
+        S[idx] = val;
+
+        if(++base_cnt == window_size + 1 )
+            base_cnt = window_size;
+
+        if(idx++==window_size-1)
+            idx = 0;
+
+        for(int i =0; i< window_size ;i++)
+            sum += S[i];
+
+        return sum/base_cnt;
     }
 };
 
