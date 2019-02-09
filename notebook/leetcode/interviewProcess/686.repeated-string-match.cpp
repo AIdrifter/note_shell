d420bdf51118293b20c3c7f28907036326bcfb08
aidrifter <aidrifter@gmail.com>
2019-02-09 17:52:16 +0800

    [Update] Google :Interview Process [122/55]
    482.license-key-formatting.cpp
            We need to understand description about question
            Think from array end.
            string s.rend()
            string s.rbegin()
    686.repeated-string-match.cpp
            string::npos = -1
            A.find(pattern) == string::npos
            => you could not find "pattern" in string A.
    
    687.longest-univalue-path.cpp
            => please find definition about subtree (Tree)

--- notebook/leetcode/interviewProcess/686.repeated-string-match.cpp
+++ notebook/leetcode/interviewProcess/686.repeated-string-match.cpp
@@ -13,20 +13,36 @@
  *
  * Given two strings A and B, find the minimum number of times A has to be
  * repeated such that B is a substring of it. If no such solution, return -1.
- * 
+ *
  * For example, with A = "abcd" and B = "cdabcdab".
- * 
+ *
  * Return 3, because by repeating A three times (“abcdabcdabcd”), B is a
  * substring of it; and B is not a substring of A repeated two times
  * ("abcdabcd").
- * 
+ *
  * Note:
  * The length of A and B will be between 1 and 10000.
- * 
+ *
  */
 class Solution {
 public:
     int repeatedStringMatch(string A, string B) {
-        
+        int repeat = 1;
+        string oriA = A; 
+        while(A.size()<=10000)
+        {
+            if(-1 != A.find(B,0))
+            {
+                //printf("find %u repeat %u \n",A.find(B,0), repeat);
+                return repeat;
+            }
+            else
+            {
+                A.append(oriA,0,oriA.size());
+                ++repeat;
+            }
+        }
+
+        return -1;
     }
 };
