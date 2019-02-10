684348236a96a8f0719b6f0d4ded89a8d2d2d948
aidrifter <aidrifter@gmail.com>
2019-02-10 13:45:20 +0800

    [Update] Google : Array and String [122/64]
    661.image-smoother.cpp
    1. Be familiar with vector of vector
    2. Array (row,col)
            a00 a01 a02
            a11 a12 a22
            a21 a22 a23
    
    485.max-consecutive-ones.cpp
    661.image-smoother.cpp

--- notebook/leetcode/arrayAndString/387.first-unique-character-in-a-string.cpp
+++ notebook/leetcode/arrayAndString/387.first-unique-character-in-a-string.cpp
@@ -11,27 +11,49 @@
  * Total Submissions: 448.1K
  * Testcase Example:  '"leetcode"'
  *
- * 
+ *
  * Given a string, find the first non-repeating character in it and return it's
  * index. If it doesn't exist, return -1.
- * 
+ *
  * Examples:
- * 
+ *
  * s = "leetcode"
  * return 0.
- * 
+ *
  * s = "loveleetcode",
  * return 2.
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * Note: You may assume the string contain only lowercase letters.
- * 
+ *
  */
 class Solution {
 public:
     int firstUniqChar(string s) {
-        
+        map<char,int> mark;
+        int min_idx = INT_MAX;
+        for(int i=0; i<s.size() ; i++)
+        {
+            if(mark.find(s[i]) == mark.end())
+                mark[s[i]] = i;
+            else
+                mark[s[i]] = -1;
+        }
+
+        // You don't know hash table map sequence
+        for(auto iter = mark.begin(); iter != mark.end() ; iter++)
+        {
+            printf(" %c %u \n",iter->first,iter->second);
+            if(iter->second!=-1)
+                if(min_idx > iter->second)
+                    min_idx = iter->second;
+        }
+
+        if(min_idx == INT_MAX)
+            return -1;
+
+        return min_idx;
     }
 };
--- notebook/leetcode/arrayAndString/661.image-smoother.cpp
+++ notebook/leetcode/arrayAndString/661.image-smoother.cpp
@@ -16,9 +16,9 @@
  * average gray scale (rounding down) of all the 8 surrounding cells and
  * itself.  If a cell has less than 8 surrounding cells, then use as many as
  * you can.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input:
  * [[1,1,1],
  * ⁠[1,0,1],
@@ -31,19 +31,51 @@
  * For the point (0,0), (0,2), (2,0), (2,2): floor(3/4) = floor(0.75) = 0
  * For the point (0,1), (1,0), (1,2), (2,1): floor(5/6) = floor(0.83333333) = 0
  * For the point (1,1): floor(8/9) = floor(0.88888889) = 0
- * 
- * 
- * 
+ *
+ *
+ *
  * Note:
- * 
+ *
  * The value in the given matrix is in the range of [0, 255].
  * The length and width of the given matrix are in the range of [1, 150].
- * 
- * 
+ *
+ *
  */
+
+/*
+ * a00 a01 a02
+ * a10 a11 a12
+ * a20 a21 a22
+ */
+
 class Solution {
-public:
-    vector<vector<int>> imageSmoother(vector<vector<int>>& M) {
-        
-    }
+    public:
+        vector<vector<int>> imageSmoother(vector<vector<int>>& M) {
+            int m = M.size();
+            int n = M[0].size();
+            vector<vector<int>> ans = M;
+            int range[] = {-1, 0, 1};
+
+            for (int row = 0; row<m ; row++)
+                for (int col = 0; col<n ; col++)
+                {
+                    int Dividend = 0;
+                    int Divisor = 0;
+                    for(int a: range)
+                        for(int b: range)
+                        {
+                            int row_now = row + a ;
+                            int col_now = col + b ;
+
+                            if(row_now>=0 && row_now<m  && col_now>=0 && col_now<n )
+                            {
+                                    Dividend += M[row_now][col_now];
+                                    Divisor++;
+                            }
+                        }
+                    ans[row][col] = (int) floor((double)Dividend/Divisor);
+                }
+
+                return ans;
+        }
 };
