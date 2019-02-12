53978f15ffbbcd3519d7d42ed161a6b3b1e00caa
aidrifter <aidrifter@gmail.com>
2019-02-12 22:07:57 +0800

    [Update] Google : Tree And Graph

---
+++ notebook/leetcode/treeAndGraph/301.remove-invalid-parentheses.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=301 lang=cpp
+ *
+ * [301] Remove Invalid Parentheses
+ *
+ * https://leetcode.com/problems/remove-invalid-parentheses/description/
+ *
+ * algorithms
+ * Hard (38.16%)
+ * Total Accepted:    106.3K
+ * Total Submissions: 278.6K
+ * Testcase Example:  '"()())()"'
+ *
+ * Remove the minimum number of invalid parentheses in order to make the input
+ * string valid. Return all possible results.
+ * 
+ * Note: The input string may contain letters other than the parentheses ( and
+ * ).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()())()"
+ * Output: ["()()()", "(())()"]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "(a)())()"
+ * Output: ["(a)()()", "(a())()"]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: ")("
+ * Output: [""]
+ * 
+ */
+class Solution {
+public:
+    vector<string> removeInvalidParentheses(string s) {
+        
+    }
+};
