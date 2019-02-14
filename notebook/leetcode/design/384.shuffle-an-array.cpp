1681cdd49c2b5e226b9c9829762965551af657e0
aidrifter <aidrifter@gmail.com>
2019-02-14 13:33:48 +0800

    [Fix] Knuth Shuffle Algo
    -- To shuffle an array a of n elements (indices 0..n-1):
         for i from 1 upto n-1 do
              j ← random integer such that 0 ≤ j ≤ i
                  exchange a[j] and a[i]
    
    [Hint] Modulo operation
    a % n = {0 , 1, 2, 3, ... n-1 }
    => a % 1 = {0}

--- notebook/leetcode/design/384.shuffle-an-array.cpp
+++ notebook/leetcode/design/384.shuffle-an-array.cpp
@@ -1,7 +1,7 @@
 /*
  * @lc app=leetcode id=384 lang=cpp
  *
- * [384] Shuffle an Array
+ * [385] Shuffle an Array
  *
  * https://leetcode.com/problems/shuffle-an-array/description/
  *
@@ -33,34 +33,80 @@
  *
  */
 class Solution {
-public:
-    vector<int> oriArray;
-    Solution(vector<int> nums) {
-        oriArray = nums;
-    }
+    public:
 
-    /** Resets the array to its original configuration and return it. */
-    vector<int> reset() {
-        return oriArray;
-    }
+        int GenRand(int low, int up)
+        {
+            srand( time(NULL) );
+            int r = rand();
+            if(up -low > 0)
+            {
+                r = r % (up -low);
+                r += low;
+            }
+            else
+                r = 0;
 
-    /** (Knuth shuffle) Returns a random shuffling of the array. */
-    /*
-     * 這裡需要注意的是i + rand() % (res.size() - i)不能寫成rand() % res.size()，
-     * 雖然也能通過OJ，但是根據這個帖子的最後部分的概率圖表，
-     * 前面那種寫法不是真正的隨機分佈
-     * https://yjk94.wordpress.com/2017/03/17/%E6%B4%97%E7%89%8C%E7%9A%84%E6%AD%A3%E7%A1%AE%E5%A7%BF%E5%8A%BF-knuth-shuffle%E7%AE%97%E6%B3%95/
-     */
-    vector<int> shuffle() { 
-       vector<int> res = oriArray;
-        int t = 0;
-        for (int i = 0; i < res.size(); ++i) {
-            if(i) t = rand()%i;
-            t = i + rand() % (res.size() - i);
-            swap(res[i], res[t]);
+            return r;
         }
-        return res;
-    }
+
+
+        vector<int> oriArray;
+        Solution(vector<int> nums) {
+            oriArray = nums;
+        }
+
+        /** Resets the array to its original configuration and return it. */
+        vector<int> reset() {
+            return oriArray;
+        }
+
+        /** (Knuth shuffle) Returns a random shuffling of the array. */
+        /*
+         * 這裡需要注意的是i + rand() % (res.size() - i)不能寫成rand() % res.size()，
+         * 雖然也能通過OJ，但是根據這個帖子的最後部分的概率圖表，
+         * 前面那種寫法不是真正的隨機分佈
+         * https://yjk94.wordpress.com/2017/03/17/%E6%B4%97%E7%89%8C%E7%9A%84%E6%AD%A3%E7%A1%AE%E5%A7%BF%E5%8A%BF-knuth-shuffle%E7%AE%97%E6%B3%95/
+         */
+
+
+        vector<int> shuffle() {
+#if 0
+            vector<int> res = oriArray;
+            int j;
+            int n = res.size();
+            for (int i = 0; i < n; i++) {
+                j = rand() % (n-i);
+                swap(res[i], res[i+j]);
+            }
+            return res;
+#elif 0
+        /*
+         *   -- To shuffle an array a of n elements (indices 0..n-1):
+         *       for i from n−1 downto 1 do
+         *            j ← random integer such that 0 ≤ j ≤ i
+         *                exchange a[j] and a[i]
+         */
+
+            vector<int> res = oriArray;
+            int j;
+            int n = res.size();
+            for (int i = n-1; i >= 1; i--) {
+                j = rand() % (i+1);
+                swap(res[i], res[j]);
+            }
+            return res;
+#else
+            vector<int> res = oriArray;
+            int j;
+            int n = res.size();
+            for (int i = 0; i < n; ++i) {
+                j = rand() % (i+1);
+                swap(res[i], res[j]);
+            }
+            return res;
+#endif
+        }
 
 
 
