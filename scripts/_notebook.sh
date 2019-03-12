1155203a655646a0f9310555e29767c528888a96
aidrifter <aidrifter@gmail.com>
2019-03-12 10:25:23 +0800

    [Fix] C++ coding template
    fix C++ coding template for codejam

--- scripts/_notebook.sh
+++ scripts/_notebook.sh
@@ -29,18 +29,43 @@ if [ ! -z "$1" ] ;then
     fi
     if [ 'cpp' = "$1" ];then
         TEMP_PATH=$PWD/main.cpp
-        echo "Quickly Generate $TEMP_PATH"
-        echo "#include <iostream>" > $TEMP_PATH
-        echo "#include <vector>" >> $TEMP_PATH
-        echo "#include <string>" >> $TEMP_PATH
-        echo "#include <map>" >> $TEMP_PATH
+        echo "#include <cstring>">$TEMP_PATH
+        echo "#include <string.h>">>$TEMP_PATH
+        echo "#include <map>">>$TEMP_PATH
+        echo "#include <deque>">>$TEMP_PATH
+        echo "#include <queue>">>$TEMP_PATH
+        echo "#include <stack>">>$TEMP_PATH
+        echo "#include <sstream>">>$TEMP_PATH
+        echo "#include <iostream>">>$TEMP_PATH
+        echo "#include <iomanip>">>$TEMP_PATH
+        echo "#include <cstdio>">>$TEMP_PATH
+        echo "#include <cmath>">>$TEMP_PATH
+        echo "#include <cstdlib>">>$TEMP_PATH
+        echo "#include <ctime>">>$TEMP_PATH
+        echo "#include <algorithm>">>$TEMP_PATH
+        echo "#include <vector>">>$TEMP_PATH
+        echo "#include <set>">>$TEMP_PATH
+        echo "#include <complex>">>$TEMP_PATH
+        echo "#include <list>">>$TEMP_PATH
+        echo "#define PB push_back">>$TEMP_PATH
+        echo "#define ALL(v) v.begin(),v.end()">>$TEMP_PATH
+        echo "#define RALL(v) v.rbegin(),v.rend()">>$TEMP_PATH
+        echo "#define REP_0(i,m) for(int i=0;i<(int)(m);i++)">>$TEMP_PATH
+        echo "#define FOR_IT(it,c) for(__typeof(c.begin()) it=c.begin();it!=c.end();++it)">>$TEMP_PATH
+        echo "#define MEMSET(a,b) memset(a,b,sizeof(a))">>$TEMP_PATH
+
         echo "using namespace std;" >> $TEMP_PATH
+        echo "class Solution" >> $TEMP_PATH
+        echo "{" >> $TEMP_PATH
+        echo "};" >> $TEMP_PATH
+
+
         echo "" >> $TEMP_PATH
         echo "" >> $TEMP_PATH
         echo "#if 1" >> $TEMP_PATH
         echo "int main()" >> $TEMP_PATH
         echo "{" >> $TEMP_PATH
-        echo "Soultion S;" >> $TEMP_PATH
+        echo "Solution S;" >> $TEMP_PATH
         echo "" >> $TEMP_PATH
         echo "    return 0;" >> $TEMP_PATH
         echo "}" >> $TEMP_PATH
