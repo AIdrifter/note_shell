241a730059913ebf1b06c9bc0a0d7cdd2faa1ae3
aidrifter <aidrifter@gmail.com>
2019-03-16 16:13:42 +0800

    [Add] Google Recursion  [122/70]
    update 2 middle question

---
+++ notebook/leetcode/recursion/247.strobogrammatic-number-ii.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=247 lang=cpp
+ *
+ * [247] Strobogrammatic Number II
+ *
+ * https://leetcode.com/problems/strobogrammatic-number-ii/description/
+ *
+ * algorithms
+ * Medium (43.69%)
+ * Total Accepted:    44.4K
+ * Total Submissions: 101.5K
+ * Testcase Example:  '2'
+ *
+ * A strobogrammatic number is a number that looks the same when rotated 180
+ * degrees (looked at upside down).
+ * 
+ * Find all strobogrammatic numbers that are of length = n.
+ * 
+ * Example:
+ * 
+ * 
+ * Input:  n = 2
+ * Output: ["11","69","88","96"]
+ * 
+ * 
+ */
+class Solution {
+public:
+    vector<string> findStrobogrammatic(int n) {
+        
+    }
+};
