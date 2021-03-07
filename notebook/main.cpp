b6c22f919976c617d329970e66478cbc62e6760a
AIdrifter <aidrifter@gmail.com>
2021-03-08 04:47:13 +0800

    [Add] Update cpp template
    update cpp template

---
+++ notebook/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define show2(str, M) \
+    cout<<str<<" "; \
+    for (auto [k,v]:M) { \
+        cout<<"("<<k<<","<<v<<") ";\
+    } \
+    cout<<endl;
+
+#define show(str, A) \
+    cout<<str<<" "; \
+    for (auto a:A) { \
+        cout<<a<<" " ;\
+    } \
+    cout<<endl; \
+
+class Solution
+{
+};
+
+
+#if 1
+int main()
+{
+Solution S;
+
+    return 0;
+}
+#endif
